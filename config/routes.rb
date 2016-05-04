e06889246ea8539dc383509d6a7f16f696325bd9
Greg Mertzlufft <greg@bikebloc.com>
2016-05-03 20:49:02 -0700

    add routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,10 @@ Rails.application.routes.draw do
   get 'call_routing/screen'
   get 'call_routing/connect'
 
+  post 'call_routing/dial'
+  post 'call_routing/screen'
+  post 'call_routing/connect'
+
   get 'number_pool/create'
   get 'number_pool/destroy'
   get 'number_pool/index'
